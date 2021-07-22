6f1fce08ad875305ca5628f54ae864667fbdc32c
Tori984 <87834922+Tori984@users.noreply.github.com>
2021-07-22 18:13:44 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,115 @@
+var clickCount = 0; 
+var justice=document.getElementById("justice");
+
+var justiceButton = document.getElementById("justiceButton");
+
+justiceButton.addEventListener("click", sendjustice);
+
+
+function sendjustice() { 
+  clickCount += 1;
+   if (clickCount == 1) {
+     //update image
+  document.getElementById("mainImages").src = "pictures/corn1.jpg";
+  sendjustice1();
+  } else if (clickCount == 2) {
+    document.getElementById("mainImages").src = "pictures/btaylor2.jpg";
+     sendjustice2();
+  } else if (clickCount == 3) {
+    document.getElementById("mainImages").src = "pictures/chris3.jpg";
+      sendjustice3();
+  } else if (clickCount == 4) {
+    document.getElementById("mainImages").src = "pictures/jam4.jpg";
+    sendjustice4();
+  } else if (clickCount == 5) {
+     document.getElementById("mainImages").src = "pictures/eli5.jpg";
+     sendjustice5();
+  } else if (clickCount == 6) {
+     document.getElementById("mainImages").src = "pictures/brown6.jpg";
+     sendjustice6();
+  } else if (clickCount == 7) {
+     document.getElementById("mainImages").src = "pictures/eric7.jpg";
+       sendjustice7();
+  } else if (clickCount == 8) {
+     document.getElementById("mainImages").src = "pictures/tamir8.jpg";
+       sendjustice8();
+    resetClickCount();
+  }
+  
+
+}
+
+function sendjustice1() {
+  justice.innerHTML= "<h1>Cornelius Fredrick</h1> Cornelius Frederick tossed a sandwich at another boy at Lakeside Academy in Kalamazoo, MI, which is run by the national for-profit company Sequel Youth and Family Services  cafeteria at lunch on April 29. Cornelius was tackled to the ground by a Lakeside employee, and seven men from the company kept him down for 12 minutes as he struggled and gradually grew still, some putting their weight on his legs and body. Cornelius' body was limp as the staff workers let go of him, according to CCTV video. Several staff stated they assumed he was faking, but several also spotted foam around his mouth, according to the police report. They dialed 911 12 minutes later. Cornelius died two days later in the hospital. Cornelius was asphyxiated, and the medical examiner deemed it a homicide. Michael Mosley, 47, and Zachary Solis, 28, and Heather McLogan, 48, were arraigned this week after being charged with involuntary manslaughter and child abuse charges have been released on bond of 500,000, court records show.";
+}
+
+function sendjustice2() {
+
+  justice.innerHTML= "<h1> Breonna Taylor</h1> According to the warrant, on March 12  Detective Joshua Jaynes of the Louisville Metro Police Department filed a request for a \"no-knock\" search warrant of Breonna Taylor's residence after investigating the actions of Jamarcus Glover, who was known to the police as a narcotics trafficker. Glover is Taylor's ex-boyfriend who was allegedly using her address to mail drugs through the post office, according to authorities. Glover was receiving packages at Taylor's address, according to police, who confirmed this with postal inspectors in the arrest warrant request. Taylor and her boyfriend, Kenneth Walker, had been sleeping in bed, both of them having no criminal history. Plainclothes Officers Myles Cosgrove and Brett Hankison, as well as Sgt. Jonathan Mattingly, uses a battering ram to smash open the green door without warning Breoana and Walker that they are the police. Walker, who later reported that he felt an intruder had broken in, fired one shot with his lawfully possessed revolver, wounding Sgt. Jonathan Mattingly in the thigh. All white and in plainclothes, Mattingly and policemen Myles Cosgrove and Brett Hankison returned fire, shooting 32 times in the dark and striking Taylor six times.Hankison was fired by the Louisville Metro Police Department on June 23 for breaking protocol, and a grand jury indicted him on three charges of wanton endangerment on September 23, after rounds he shot entered an adjacent apartment with people inside, according to an internal inquiry. He entered a not guilty plea. Mattingly and Cosgrove were justified in returning fire and killing Breoana Taylor , according to Kentucky Attorney General Daniel Cameron. There were no charges filed against either man.";
+}
+
+function sendjustice3() {
+  justice.innerHTML= " <h1> Christopher Mitchell </h1> Christopher Mitchell was pulled over on December 9th 2018 by Torrance City Police. Officers responded to a call about a stolen vehicle.  Authorities said he was armed with a rifle and in a stolen vehicle. Police later clarified the alleged rifle was a toy air rifle. The family feels the police already had in their minds they wanted to shoot Mitchell because within fifteen seconds of making contact with him, they were already shooting. When viewing the video you can see  Mitchell had his hands up, complying with the officers.He was completely compliant and respectful to both officers even as he was being mistreated. The officer on duty fired three shots into Mitchell’s car, killing him instantly.";
+}
+function sendjustice4() {
+  justice.innerHTML= "<h1> Jamarion Robinson </h1> Jamarion Robinson was only 26 years old when members of the Metro Atlanta Fugitive Task Force conducted a raid at his girlfriend's East Point apartment. They were attempting to serve an arrest warrant for allegedly shooting at an Atlanta PD officer a week prior. U.S Marshals along with the task force entered the apartment using force and opened fire on Jamarion 95 times 76 of which entered his body.He was unarmed.They threw grenades at him and burnt his body.It was also concluded that 2 shots were fired by an officer standing over him while he was already on the floor. They then proceeded to drag his handcuffed body down the stairs. His body laid lifeless in the apartment for 8 hours. After his death the police showed family members a picture of the suspect they were looking for and t was not Jamain.  Officer William T. Saul, an officer involved with Jamian's death was an officer making headlines for his involvement in the tasing and arrest of two AUC center students ,Taniyah Pilgrim and Messiah Young (broadcasted on CNN) during the recent BLM protests in Atlanta. He has also been implicated in the involvement of the shooting of Jamarion. DA Paul Howard's office has been working on the case since his death but The US Marshals refused to talk. ";
+}
+  function sendjustice5() {
+  justice.innerHTML=  "<h1>Eliijah McClain </h1> August 24th, 2019, 23-year-old Elijah McClain was walking home after buying some tea at a local convenience store. According to Elijah's family, Elijah, who was anemic, preferred to wear a ski mask to keep his face warm while he was walking. The Aurora Police Department received a call of a \"suspicious man\". Elijah who was not armed and had not committed any kind of crime — he “resisted arrest.” In the 15 minutes that followed, the officers tackled McClain to the ground, put him in a carotid hold,  \"He is laying on the ground vomiting, he is begging, he is saying, 'I can't breathe.' One of the officers says, 'Don't move again. If you move again, I'm calling in a dog to bite you,'\" footage from the bodycam. When the paramedics arrived they administered a dose of Ketamine to the already unconscious Elijah to \"sedate\" him. He was pronounced dead August 27th, 2019, at only 23 years old of a heart attack. The officers on the scene were placed on temporary leave, however, they are back in the field with no charges. ";
+  }
+  function sendjustice6() {
+  justice.innerHTML= "<h1> Micheal Brown </h1> On 9 August 2014,. Mr Wilson was driving a police car and encountered Micheal and a friend, Darren Johnson walking down a street. Stopping Mr Brown and his friend Dorian Johnson for walking in the middle of the road, the policeman grew suspicious that he had carried out the robbery. Officer Wilson, the officer involved, has not spoken publicly about the incident, and there has been no clear video of the shooting. So we don't actually know the whole story. Wilson syas Brown attacked Wilson in his police vehicle, according to Wilson, for control of Wilson's gun until it was fired, resulting in an incident. According to Darren Johnson , Wilson allegedly started the fight by grasping Brown's neck through his car window, threatening him, and then shooting at him.. Both Wilson and Johnson claim that Brown and Johnson ran at this moment, with Wilson pursuing Brown shortly after. After a brief chase, Wilson claims Brown came to a halt and charged him.  Johnson contradicted this account, claiming that after Wilson fired a shot at Brown's back, he turned around with his hands raised. Wilson then shot Brown multiple times until he collapsed to the ground, according to Johnson. Wilson fired a total of twelve bullets throughout the incident, including twice during the struggle in the car, the last of which was likely the fatal shot. Brown was hit six times in the front of his torso, all in the same spot. Tiffany Mitchell, a witness, told MSNBC that Wilson fired a shot at Brown as he ran away, and that Brown's body jolted as though he had been shot. Brown \"turned around and put his hands up, and the officer continued to approach him and shot him until he went all the way down to the ground,\" according to Mitchell. Another witness, Piaget Crenshaw, offered a similar description. Brown's lifeless body lay on the street for four hours in the scorching heat of June.  A grand jury was called and given extensive evidence from Robert McCulloch, the St. Louis County Prosecutor. On November 24, 2014, McCulloch announced the St. Louis County grand jury had decided not to indict Wilson. A new St. Louis prosecutor, Wesley Bell, spent five months in 2020 reviewing the case with an eye to charging Wilson with either manslaughter or murder. In July, Bell announced he would not charge Wilson with any crime";
+  }
+  function sendjustice7() {
+  justice.innerHTML= " <h1> Eric Garner </h1>  On July 17, 2014, Two NYPD officers challenged Eric Garner, a 43-year-old African American father of six, for unlawfully selling cigarettes. Justin Damico, one of the policemen,  with Daniel Pantaleo. As they pushed in, a buddy of Mr. Garner's cell phone camera captured the struggle that would soon be viewed by millions, the chokehold. Officers in a swarm. The Eric cries for air, \"I can't breathe,\" Mr. Garner's final words became a rallying cry for a protest movement.  The two plainclothes officers came to the situation first, and the TWO NYPD officers  who arrived later never regained control. As officers held Mr. Garner down on the sidewalk, two witnesses said they heard a sergeant order the officers to lighten up. Rodney Lee,  business manager, recounted hearing the sergeant remark that day, \"Let up.\" “You already have him.\” Mr. Garner laid on the ground for minutes without receiving oxygen from the responding emergency medical workers from Richmond University Medical Center. He eventually went unconscious. He was pronounced dead at a hospital about an hour later, and his death was ruled a homicide by suffocation by the medical examiner. The following year, when New York State repealed its ban on publicizing police disciplinary records, it was revealed that Pantaleo had been investigated for misconduct seven times in the five years before Garner's death In the only inquiry completed so far, a Staten Island grand jury found Officer Pantaleo committed no crime when he used a chokehold — a technique banned by police rules.";
+  }
+  function sendjustice8() {
+  justice.innerHTML= "<h1> Tamir Rice </h1> On November 22, 2014, Cleveland Police station received a call that told dispatchers that a “juvenile” is “pulling a gun in and out of his pants and pointing it at people.” The caller later added, “It’s probably fake.” But the 911 dispatcher never told officers that Rice was a kid or that the gun was likely fake. Tamir Rice was throwing snowballs and playing with a toy pellet gun in a Cleveland park when a police car rolled into the snowy field,  Video footage of the killing  shows the child was shot within two seconds of the police arriving at the scene. Loehmann, who fired two bullets into Rice, and Garmback allege they believed he was carrying a gun. In fact, the boy was playing with a toy air pellet gun near a playground at a city recreation center. Seconds later, the boy lay dying from a police officer’s bullet. “Shots fired, male down,” one of the officers in the car called across his radio. “Black male, maybe 20, black revolver, black handgun by him. Send E.M.S. this way, and a roadblock.”  Garmback quickly reported the shooting to dispatch and requested emergency personnel respond to the scene. But the officers, neither of whom reportedly had first aid kits or training, then stood around without applying first aid for about four minutes after Rice was shot. It wasn’t until an FBI agent — a trained paramedic — walked onto the scene that Rice received first aid. Rice acknowledged the FBI agent, showing signs of life as the agent tried to tend to the boy’s wounds without any medical tools. “He turned over and acknowledged and looked at me, and he, like, reached for my hand,” the agent said, later adding that Rice said his name and mumbled something about the pellet gun. Paramedics arrived a few minutes later. They eventually took Rice to the hospital, where he died on November 23, 2014. The officers on scene faced no chargers." ;
+
+  }
+function resetClickCount() {
+  clickCount = 0;
+}
+
+
+
+
+
+/*
+function sendMotivation1() {
+  alert("Deep breaths. You got this.");
+}
+
+function sendMotivation2() {
+  alert("Be nice to people. We're all trying our best.");
+}
+
+function sendMotivation3() {
+  alert("I'm proud of the progress you're making. No matter how small.");
+}
+
+function resetClickCount() {
+  clickCount = 0;
+}
+*/
+
+/* TODO: Add a variable to store the "my-list" element */
+var fullItemList = document.getElementById("my-list"); 
+
+
+/* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
+fullItemList.addEventListener("click", checkOffItem);
+
+/* TODO: Declare the function checkOffList and add actions inside the { } */
+
+if (fullItemList) {
+  fullItemList.addEventListener("click", checkOffItem);
+}
+
+function checkOffItem(clicked) {
+    if (clicked.target.tagName == "LI") {
+        clicked.target.classList.toggle("all-done");
+    }
+}
+
+
